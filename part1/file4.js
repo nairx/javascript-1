bff6b5725bab3fe70561469fc8346258d5af8eb1
nairx <nairxprv@gmail.com>
2026-02-11 12:34:21 +0530

    updated

--- part1/file4.js
+++ part1/file4.js
@@ -1,12 +1,13 @@
 let a = 10;
-let b = 5;
+let b = 10;
 // if (a > b) {
 //   console.log("a is greater");
 // } else {
 //   console.log("b is greater");
 // }
-
 if (a > b) 
     console.log("a is greater");
-else 
+else if (b > a) 
     console.log("b is greater");
+else
+    console.log("Both are equal")
