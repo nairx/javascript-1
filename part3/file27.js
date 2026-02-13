36b0250a50fdc7a41403f07754e3c8fb3142b6e4
nairx <nairxprv@gmail.com>
2026-02-13 10:38:04 +0530

    Updated

---
+++ part3/file27.js
@@ -0,0 +1,21 @@
+// let score = 90
+// let result
+// if (score>50){
+//     result = "Pass"
+// }
+// else{
+//     result = "Fail"
+// }
+// console.log(result)
+
+// let score = 90
+// let result = score > 50 ? "Pass" : "Fail"
+// console.log(result)
+
+// let score = 9
+// let result = score > 50 && "Pass" 
+// console.log(result)
+
+let score
+let result = score || 30 
+console.log(result)
